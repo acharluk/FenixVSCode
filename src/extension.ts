1be2f54c55ce318913934a9226d8089a68a1fc9c
ACharLuk <acharluk@gmail.com>
2020-03-16 16:05:18 +0100

    Basic parsing

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,6 @@
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
 
@@ -12,7 +14,35 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 		);
 
-		panel.webview.html = `<h1>Hello Fenix!</h1>`;
+		const nextVar = (text: string) => {
+			return text.match(/\$var{\s*(?<var_name>.+?)\s*}/);
+		};
+
+		let page = fs.readFileSync(path.join(context.extensionPath, 'views', 'main.fnx')).toString();
+		const data: any = {
+			msg: 'Hello parsed!',
+			click: 'Click me!'
+		};
+
+		let vars = nextVar(page);
+		while (vars !== null) {
+			if (vars.groups) {
+				page = page.replace(vars[0], data[vars.groups.var_name]);
+			}
+
+			vars = nextVar(page);
+		}
+
+		panel.webview.html = `
+		<!DOCTYPE html>
+		<html lang="en">
+			<head>
+				<script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+			</head>
+			<body>
+				${page}
+			</body>
+		</html>`;
 	});
 
 	context.subscriptions.push(disposable);
