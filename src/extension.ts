d6ffbf4ed3622bd9b8cf0d80828064e7ca3d79c3
ACharLuk <acharluk@gmail.com>
2020-03-16 13:25:50 +0100

    Basic webview

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,16 @@ import * as vscode from 'vscode';
 export function activate(context: vscode.ExtensionContext) {
 
 	let disposable = vscode.commands.registerCommand('fenix.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World!');
+		const panel = vscode.window.createWebviewPanel(
+			'fenix',
+			'Fenix: New project',
+			vscode.ViewColumn.One,
+			{
+				enableScripts: true
+			}
+		);
+
+		panel.webview.html = `<h1>Hello Fenix!</h1>`;
 	});
 
 	context.subscriptions.push(disposable);
